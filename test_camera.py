fbe8c654b6c0aed3928667c5bae983b880328cce
PANDURANGZURE <pandurangzure6@gmail.com>
2025-08-30 11:57:04 +0530

    a

---
+++ test_camera.py
@@ -0,0 +1,35 @@
+from flask import Flask, Response
+
+import cv2
+
+app = Flask(__name__)
+
+def gen_frames():
+    cap = cv2.VideoCapture(0)
+    while True:
+        success, frame = cap.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/')
+def index():
+    return '''
+    <html>
+        <body>
+            <h1>Live Camera Feed</h1>
+            <img src="/video_feed" width="640" height="480">
+        </body>
+    </html>
+    '''
+
+if __name__ == '__main__':
+    app.run(debug=True)
